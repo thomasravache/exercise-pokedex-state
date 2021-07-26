7b17ee74328c5d339b89b4143fb61064215a0c47
thomasravache <thomasravache31@gmail.com>
2021-07-26 13:39:44 -0300

    Exercicio 02 - OK

--- src/App.js
+++ src/App.js
@@ -4,35 +4,11 @@ import pokemons from './data';
 import Pokedex from './Pokedex';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      pokeIndex: 0,
-    }
-    this.handlerClick = this.handlerClick.bind(this);
-  }
-
-  handlerClick() {
-    if(this.state.pokeIndex >= pokemons.length-1) {
-      this.setState({
-        pokeIndex: 0,
-      });
-    } else {
-      this.setState((prevState, _props) => ({
-        pokeIndex: prevState.pokeIndex + 1,
-      }));
-    }
-  }
-
   render() {
-    const { pokeIndex } = this.state;
-
     return (
       <div className="App">
         <h1> Pokedex </h1>
-        <Pokedex pokemons={pokemons} pokeIndex={ pokeIndex } />
-        <button onClick={ this.handlerClick }>Pokemon {pokeIndex+1}</button>
+        <Pokedex pokemons={pokemons} />
       </div>
     );
   }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,66 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          pokeIndex: 0,
+          typeOfPokemon: 'All',
+        }
+        this.nextPokemonClick = this.nextPokemonClick.bind(this);
+        this.filteredPokemons = this.filteredPokemons.bind(this);
+        this.changeState = this.changeState.bind(this);
+        this.getTypesOfPokemons = this.getTypesOfPokemons.bind(this);
+      }
+
+    filteredPokemons(typeOfPokemon) {
+        if(typeOfPokemon === 'All') return this.props.pokemons;
+
+        return this.props.pokemons
+            .filter((pokemon) => pokemon.type === typeOfPokemon);
+    }
+
+    changeState(event) {
+        this.setState({
+            pokeIndex: 0,
+            typeOfPokemon: event.target.innerText,
+        })
+    }
+
+    getTypesOfPokemons() {
+        const allTypes = this.props.pokemons
+            .map((pokemon) => pokemon.type);
+        return [...new Set(allTypes)]; // removendo tipos repetidos
+    }
+
+    nextPokemonClick() {
+        const { pokeIndex, typeOfPokemon } = this.state;
+
+        if(pokeIndex >= this.filteredPokemons(typeOfPokemon).length-1) {
+            this.setState({
+            pokeIndex: 0,
+            });
+        } else {
+            this.setState((prevState, _props) => ({
+            pokeIndex: prevState.pokeIndex + 1,
+            }));
+        }
+    }
+
     render() {
-        const { pokeIndex, pokemons }  = this.props;
+        const { pokeIndex, typeOfPokemon } = this.state;
+        const pokemon = this.filteredPokemons(typeOfPokemon)[pokeIndex];
+        const typeOfPokemons = this.getTypesOfPokemons();
 
         return (
             <div className="pokedex">
                 {/* {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)} */}
-                <Pokemon pokemon={pokemons[pokeIndex]} />
+                <Pokemon pokemon={ pokemon } />
+                <div>
+                    { typeOfPokemons.map((type) => <button key={ type } onClick={ this.changeState }>{ type }</button>) }
+                    <button onClick={ this.nextPokemonClick }>Próximo Pokémon</button>
+                </div>
             </div>
         );
     }
